84911d6bc497205d92ba8e8c6cca981897be33a9
Gunnar Sinn <gsfd2000@gmail.com>
2021-08-20 12:27:09 +0200

    k3d cluster launch completed

--- cluster-setup/k3d/install_k3d.sh
+++ cluster-setup/k3d/install_k3d.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
-export KUBECONFIG=$PWD/kubeconfig.yaml
+curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh > install.sh
+# PATH=/usr/local/bin:$PATH
+
+# shit redhat docker installation problem creator removal
+sudo yum remove -y podman
+sudo yum remove -y buildah
+
+# docker installation
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+sudo usermod -a -G docker vagrant
+
+sudo systemctl start docker
+sudo systemctl status docker --no-pager
+
+#kubectl installation
+sudo cp ./kubernetes.repo /etc/yum.repos.d
+sudo yum install -y kubectl
+kubectl version
+
+# k3d installation
+bash install.sh
+# export KUBECONFIG=$PWD/kubeconfig.yaml
+k3d version
