c2098fcf1d7f905f45db73409f4481475378947d
gsfd2000 <gsfd2000@gmail.com>
2021-09-03 10:15:45 +0200

    added kubernetes base app config change of BASE_HOST

--- cluster-setup/k3d/set_env.sh
+++ cluster-setup/k3d/set_env.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Source: https://gist.github.com/48f44d3974db698d3127f52b6e7cd0d3
 
 ###########################################################
@@ -6,6 +7,10 @@
 # https://youtu.be/XNXJtxkUKeY                            #
 ###########################################################
 
+# run via command 
+# source ./set_env.sh to execute in actual shell
+# https://stackoverflow.com/questions/18547881/shell-script-to-set-environment-variables
+
 # Requirements:
 # - k8s v1.19+ cluster with nginx Ingress
 
@@ -13,21 +18,21 @@
 export GH_ORG=gsfd2000
 
 # Replace `[...]` with the base host accessible through NGINX Ingress
-export BASE_HOST=[...] # e.g., $INGRESS_HOST.nip.io
+export BASE_HOST=argo.192.168.62.2.nip.io
 
 export REGISTRY_SERVER=https://index.docker.io/v1/
 
 # Replace `[...]` with the registry username
-export REGISTRY_USER=[...]
+export REGISTRY_USER=gsfd2000
 
 # Replace `[...]` with the registry password
-export REGISTRY_PASS=[...]
+export REGISTRY_PASS='Federica&27'
 
 # Replace `[...]` with the registry email
-export REGISTRY_EMAIL=[...]
+export REGISTRY_EMAIL=gsfd2000@gmail.com
 
 # Replace `[...]` with the GitHub token
-export GH_TOKEN=[...]
+export GH_TOKEN=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
 
 # Replace `[...]` with the GitHub email
-export GH_EMAIL=[...]
+export GH_EMAIL=gsfd2000@gmail.com
